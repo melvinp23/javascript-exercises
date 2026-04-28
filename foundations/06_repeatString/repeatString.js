2a3e32feea8d57ffc4ff41ada4fee06cb9e0a175
melvin <melvin.pamplona@gmail.com>
2026-04-29 00:39:58 +0800

    Complete all test on repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,12 +1,19 @@
 const repeatString = function (string, num) {
 	let result = '';
+
 	for (let i = 0; i < num; i++) {
 		result += string;
 	}
-	return result;
+
+	if (num < 0) {
+		result = 'ERROR';
+		return result;
+	} else {
+		return result;
+	}
 };
 
-console.log(repeatString('yo ', 3));
+console.log(repeatString('', 10));
 
 // Do not edit below this line
 module.exports = repeatString;
