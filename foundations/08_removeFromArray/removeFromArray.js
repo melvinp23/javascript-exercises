a7211177a16d120430f2fc866e3d044a474d3663
melvin <melvin.pamplona@gmail.com>
2026-04-30 23:43:56 +0800

    Finish and test removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function (arr, ...args) {
 	return arr.filter(val => !args.includes(val));
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 3, 4));
+console.log(removeFromArray([1, 'ho', 2, 3], '1', 'ho'));
 
 // Do not edit below this line
 module.exports = removeFromArray;
