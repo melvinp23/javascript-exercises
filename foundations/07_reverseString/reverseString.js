f98ccfb8dfb39e771855b206186d496b0ad5e266
melvin <melvin.pamplona@gmail.com>
2026-04-30 22:25:21 +0800

    Finish reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+	return str.split('').reverse().join('');
 };
+
+string = '';
+
+console.log(reverseString(string));
 
 // Do not edit below this line
 module.exports = reverseString;
