22288a476418e2f20839d012a7b00bcc4e20b088
melvin <melvin.pamplona@gmail.com>
2026-05-01 14:25:00 +0800

    Finish and test leap years exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+	const isYearDivisibleByFourHundred = year % 400 === 0;
+	const isYearDivisibleByFour = year % 4 === 0;
+	const isCentury = year % 100 === 0;
 
+	if (isYearDivisibleByFour && (isYearDivisibleByFourHundred || !isCentury)) {
+		return true;
+	} else return false;
 };
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
