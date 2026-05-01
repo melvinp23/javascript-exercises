91599897d36b84a4e9e6dcaaf9020178eb55188c
melvin <melvin.pamplona@gmail.com>
2026-05-01 15:01:17 +0800

    Finish and test temperature conversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+	const convertedCelsius = (temp - 32) / 1.8;
+	return Math.round(convertedCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+	const convertedFahrenheit = temp * 1.8 + 32;
+	return Math.round(convertedFahrenheit * 10) / 10;
 };
+
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
