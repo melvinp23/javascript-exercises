028bc0478fc9523b25c43c0fc39cd18192b04a1d
melvin <melvin.pamplona@gmail.com>
2026-05-12 02:28:28 +0800

    Finish calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,61 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+add(0, 0);
+add(2, 2);
+add(5, 24);
+
+const subtract = function (num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+subtract(10, 4);
+subtract(-10, -4);
+subtract(-8, 5);
+
+const sum = function (arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+};
+sum([]);
+sum([7]);
+sum([7, 11]);
+sum([1, 3, 5, 7, 9]);
+
+const multiply = function (arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const multiply = function() {
+multiply([2, 4]);
+multiply([2, 4, 6, 8, 10, 12, 14]);
 
+const power = function (base, exponent) {
+	return base ** exponent;
 };
 
-const power = function() {
-	
+power(4, 3);
+power(3, 10);
+
+const factorial = function (n) {
+	let res = 1;
+	for (let i = 1; i <= n; i++) {
+		res *= i;
+	}
+	return res;
 };
 
-const factorial = function() {
-	
-};
+factorial(0);
+factorial(1);
+factorial(2);
+factorial(5);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
