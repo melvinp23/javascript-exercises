a46ed9230f182353b607ced641ff4334d886698d
melvin <melvin.pamplona@gmail.com>
2026-05-12 15:17:10 +0800

    Finish getTheTitles exercises using arr.map() method

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const books = [
+	{
+		title: 'Book',
+		author: 'Name',
+	},
+	{
+		title: 'Book2',
+		author: 'Name2',
+	},
+];
 
+const getTheTitles = function (arr) {
+	const bookTitles = arr.map(book => book.title);
+	return bookTitles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+console.log(getTheTitles(books));
