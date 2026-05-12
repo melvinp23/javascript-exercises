e2a89c893a97f4e987f201f204c971335d91327c
melvin <melvin.pamplona@gmail.com>
2026-05-12 12:00:08 +0800

    Finish palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	let cleanString = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+	let reverseString = cleanString.split('').reverse().join('');
+
+	return cleanString === reverseString;
 };
+
+console.log(palindromes('racecar!'));
+console.log(palindromes('rac3e3car'));
+console.log(palindromes('A car, a man, a maraca.'));
+console.log(palindromes('racecar!'));
+console.log(palindromes('Racecar!'));
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
+console.log(palindromes('r3ace3car'));
 
 // Do not edit below this line
 module.exports = palindromes;
